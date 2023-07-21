acdfe4895d754a53020f329a7a14f625ae28670a
Никита <ikarus11122@gmail.com>
2023-07-21 08:33:35 +0300

    Initial commit

---
+++ src/test/java/ru/gb/fitnessclub/gateway/GateWayApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.gb.fitnessclub.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GateWayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
